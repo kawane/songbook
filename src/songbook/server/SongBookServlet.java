20bcae785c76950904afb6f36e11a739ccf952f5
Laurent Le Goff <legoff.laurent@gmail.com>
2014-04-23 23:13:27 +0200

    Create intellij project

---
+++ src/songbook/server/SongBookServlet.java
@@ -0,0 +1,19 @@
+package songbook.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by LF5 on 23/04/2014.
+ */
+public class SongBookServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
