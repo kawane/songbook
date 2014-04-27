e25e29681718101cbbfff28c17b90fcdbf4d3b58
Jean-Charles Roger <jeancharles.roger@gmail.com>
2014-04-27 21:00:35 +0200

    Shows song list.

--- src/songbook/server/SongBookServlet.java
+++ src/songbook/server/SongBookServlet.java
@@ -6,6 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by LF5 on 23/04/2014.
@@ -13,9 +18,27 @@ import java.io.PrintWriter;
 public class SongBookServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String pathInfo = request.getPathInfo();
-        PrintWriter out = response.getWriter();
-        out.print(pathInfo);
+        final PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Songbook</title></head>");
+        out.println("<body>");
+        try {
+            final Connection connection = Database.getConnection();
+            final Statement statement = connection.createStatement();
+            final boolean execute = statement.execute("SELECT t.* FROM songbook.Songs t", new String[]{"id", "name"});
+
+            final ResultSet resultSet = statement.getResultSet();
+
+            out.println("<ul>");
+            while ( resultSet.next() ) {
+                out.println("<li>" + resultSet.getString(2) + "</li>");
+            }
+            out.println("</ul>");
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
+        out.println("</body>");
+        out.println("</html>");
     }
 
 }
