1acaba3fbaf329a680754c9acea975f419ccf393
Jean-Charles Roger <jeancharles.roger@gmail.com>
2014-04-29 23:09:42 +0200

    Updates song table and adds a user table

--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,20 @@
+DROP TABLE  songbook.Songs;
 CREATE TABLE songbook.Songs
 (
-  id int PRIMARY KEY NOT NULL,
-  name text NOT NULL,
-  author text,
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name varchar(256) NOT NULL,
+  author varchar(256),
   creator int NOT NULL,
   contents text
 );
 ALTER TABLE songbook.Songs ADD CONSTRAINT unique_id UNIQUE (id);
+
+DROP TABLE  songbook.Users;
+CREATE TABLE songbook.Users
+(
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT ,
+  login varchar(256)  NOT NULL,
+  password varchar(256) NOT NULL,
+  salt varchar(10) NOT NULL
+);
+ALTER TABLE songbook.Users ADD CONSTRAINT unique_id UNIQUE (id);
