38a803da91881d83713e852457379a49192aeff8
Jean-Charles Roger <jeancharles.roger@gmail.com>
2014-10-11 17:00:19 +0200

    Corrects deploy script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export SOURCES=/Volumes/diskE/Users/j5r/Documents/Personnel/dev/git/songbook
-export DEPLOY=/Users/j5r/Documents/Personnel/dev/git/openshift/songbook
+export SOURCES=/Volumes/diskE/Users/j5r/Documents/Personnel/dev/kawane/songbook
+export DEPLOY=/Volumes/diskE/Users/j5r/Documents/Personnel/dev/kawane/openshift/songbook
 
 rm -fr $DEPLOY/songbook
 cp -R $SOURCES/out/artifacts/songbook/ $DEPLOY/songbook/
