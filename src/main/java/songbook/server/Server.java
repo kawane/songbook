693de590423f9c56bdd21cd77eef8ad4147a0d72
Laurent Le Goff <legoff.laurent@gmail.com>
2015-04-30 21:53:11 +0200

    fix new layout errors

--- src/main/java/songbook/server/Server.java
+++ src/main/java/songbook/server/Server.java
@@ -286,14 +286,14 @@ public class Server {
 			if (songContents == null) throw new SongNotFoundException(id);
 			String title = SongUtils.getTitle(songContents);
 			Templates.header(out, "Edit - " + title + " - My SongBook", role);
-			Templates.editSong(out, id, songContents);
+			Templates.editSong(out, id, songContents, role);
 			Templates.footer(out);
 
 			exchange.getResponseSender().send(out.toString());
 
 		} else {
 			Templates.header(out, "Create Song - My SongBook", role);
-			Templates.editSong(out, "", Templates.newSong(new StringBuilder()));
+			Templates.editSong(out, "", Templates.newSong(new StringBuilder()), role);
 			Templates.footer(out);
 			exchange.getResponseSender().send(out.toString());
 		}
@@ -320,13 +320,14 @@ public class Server {
 			case MIME_TEXT_HTML:
 				exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
 
-				Templates.header(out, title, getRole(exchange));
+				String role = getRole(exchange);
+				Templates.header(out, title, role);
 				if (showKeyCreationAlert) {
 					Templates.alertKeyCreation(out, administratorKey, exchange.getRequestPath());
 				}
 				StringBuilder result = new StringBuilder();
 				indexDb.search(query, result, mimeType);
-				Templates.search(out,result);
+				Templates.search(out,result, role);
 
 				Templates.footer(out);
 				break;
@@ -386,9 +387,10 @@ public class Server {
 		StringBuilder out = new StringBuilder();
 		// Todo use a songmark object to extract title and then generate html
 		String title = SongUtils.getTitle(songData);
+		String role = getRole(exchange);
 		Templates.header(out, title + " - My SongBook", getRole(exchange));
 		if (showKeyCreationAlert) Templates.alertKeyCreation(out, administratorKey, path);
-		Templates.viewSong(out, id, SongUtils.writeHtml(new StringBuilder(), songData));
+		Templates.viewSong(out, id, SongUtils.writeHtml(new StringBuilder(), songData), role);
 
 		Templates.footer(out);
 		return out.toString();
--- src/main/java/songbook/server/Templates.java
+++ src/main/java/songbook/server/Templates.java
@@ -33,15 +33,15 @@ public class Templates {
 	}
 
     public static <A extends Appendable> A search(A out, CharSequence searchResult, CharSequence role) {
-        return print(out, "search.html", "searchResult", searchResult);
+        return print(out, "search.html", "searchResult", searchResult, "role", role);
     }
 
     public static <A extends Appendable> A editSong(A out, CharSequence songId, CharSequence song, CharSequence role) {
-        return print(out, "editSong.html", "songId", songId, "song", song);
+        return print(out, "editSong.html", "songId", songId, "song", song, "role", role);
     }
 
     public static <A extends Appendable> A viewSong(A out, CharSequence songId, CharSequence song, CharSequence role) {
-        return print(out, "viewSong.html", "songId", songId, "song", song);
+        return print(out, "viewSong.html", "songId", songId, "song", song, "role", role);
     }
 
     public static <A extends Appendable> A newSong(A out) {
